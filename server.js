2624f74f0af30366f96a2f9a009c609bada393e6
ilnuribat <ilnuribat@gmail.com>
2014-07-03 00:53:28 +0400

    makeQueue - simple queue example

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var Var = require('./variables.js');
 var get = require('./getServer.js');
 var post = require('./postServer.js');
-var QueueExe = require('./makeQueue.js');
+var queue = require('./makeQueue.js');
 
 get.Data;
 get.Get;
@@ -11,8 +11,7 @@ post.Post;
 post.qDriver;
 post.qPassanger;
 
-QueueExe.findQueue;
+queue.find;
 
 Var.app.listen(80);
-
 console.log("Server started");
