8a410c1abdf15e6a01da7e039982d61829bd751a
ilnuribat <ilnuribat@gmail.com>
2014-07-15 10:14:39 +0600

    independent script for working with dataBase

--- db.js
+++ db.js
@@ -3,24 +3,23 @@ var mysql = require('mysql');
 
 //create connection to MySql Server and Data Base
 
-var connection = mysql.createConnection( {
-  host: 'localhost', 
-  port: 80,
-  database: 'mb',
-  user: 'Ilnur', 
-  password: 'Ilnur'
-});
+var connection = null;
 
 //Create a simple Web Server to respond to requests
 
 http.createServer(function(request, response) {
   //RECIEVED A REQUEST
   //for this example respond with a HTTP 200 OK
-  
-  response.writeHeader(200);
   response.write('Connect to mySql\n');
   
   //Connect to mySql (if there is an error, report and terminate the request
+  connection = mysql.createConnection( {
+    host: 'localhost', 
+    port: 3306,
+    database: 'server',
+    user: 'root', 
+    password: ''
+  });
   
   connection.connect(function(error) {
     if(error != null) {
@@ -30,20 +29,22 @@ http.createServer(function(request, response) {
   
   //Query the data to some data
   
-  connection.query("SELECT * from mb.g WHERE ref = '806787088'", function(error, rows){
+  connection.query("SELECT * from direction WHERE 1", function(error, rows){
     //There was a error or not?
     
     if(error != null) {
       response.end('Query error:' + error);
     } else {
       //Shows the result on console window
-      console.log(rows[0]);
+      for(var i in rows)
+        console.log(rows[i]);
       response.end('Success');
     }
+    
     //close connection
     connection.end();
   });
   
-  //The server will be listen on port 90
+  //The server will be listen on port 80
   
 }).listen(80);
