a25e18c2e992d8012720b5724b8176b6da9f7404
ilnuribat <ilnuribat@gmail.com>
2014-09-04 03:40:22 +0600

    80 port

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ var post = require('./postServer.js');
 var init = require('./init.js');
 var queue = require('./makeQueue.js');
 
-Var.app.listen(8080);
+Var.app.listen(80);
 console.log("Server started");
