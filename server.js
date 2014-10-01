3fae8c21209f2554474ac0a5577cc85817dbe51b
ilnuribat <ilnuribat@gmail.com>
2014-10-01 16:59:52 +0500

    nothing done

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var get = require('./getServer.js');
 var post = require('./postServer.js');
 var init = require('./init.js');
 var queue = require('./makeQueue.js');
-
 Var.app.listen(80);
 console.log("Server started");
 //ilnur
