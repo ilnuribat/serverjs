25d74a076a30dd447204a967b6a8b69dbf19630f
ilnuribat <ilnuribat@gmail.com>
2014-11-10 18:38:29 +0400

    disable makeQueue

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var Var = require('./variables.js');
 var get = require('./getServer.js');
 var post = require('./postServer.js');
 var init = require('./init.js');
-var queue = require('./makeQueue.js');
+//var queue = require('./makeQueue.js');
 Var.app.listen(80);
 console.log("Server started");
 //ilnur
