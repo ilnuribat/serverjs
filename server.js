d0b8e487092c91a9b713712c48c827d6d375cfc2
bulat <m11e@yandex.ru>
2014-11-17 15:50:32 +0600

    some problems on develop machine with standart 80 port

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var get = require('./getServer.js');
 var post = require('./postServer.js');
 var init = require('./init.js');
 var queue = require('./makeQueue.js');
-Var.app.listen(80);
+Var.app.listen(8080);
 console.log("Server started");
 //ilnur
