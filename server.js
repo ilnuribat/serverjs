d29b476aaca4fed2908c3837990927ad1e7a9936
ilnurbat <ilnurbat@gmail.com>
2014-11-27 12:57:26 +0500

    start makeQueue.find()

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var Var = require('./variables.js');
 require('./getServer.js');
 require('./postServer.js');
 require('./init.js');
-require('./makeQueue.js');
+require('./makeQueue.js').find();
 Var.app.listen(8080);
 console.log("Server started");
+
