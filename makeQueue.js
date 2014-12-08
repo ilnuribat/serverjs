2544d8bf6ab07886bff4ecd583593556ddd30e33
ilnurbat <ilnurbat@gmail.com>
2014-12-09 03:25:41 +0500

    select unique days, where we have someone on queue

--- makeQueue.js
+++ makeQueue.js
@@ -13,73 +13,79 @@ var sql = require('./sql.js');
  * */
 
 //встречаем людей
-var find = function find_queue() {
+var mainF = function main() {
     setTimeout(find_queue, 3000);
     clear();
     //Очередь по любому чистая, по крайней мере, после вызова clear();
-
-    //Цикл по всем направлениям.
-    for(var direction = 1; direction <= Var.directionSize; direction ++){
-        //Цикл по временам - на каждом интервале по 2 очереди: пассажиры и водители
-        //всего 8 интервалов времени с 00:00 - 03:00, 03:00 - 06:00, и т.д с шагом в 3 часа
-        for(var TIME = 1; TIME <= 8; TIME ++){
-            //Если в очереди никого нет
-            if(Var.qDriver[direction][TIME].length * Var.qPassenger[direction][TIME].length == 0) {
-            } else
-                console.log("time: ", TIME, " direction: ", direction, " queue's size: ", 
+    
+    /*
+     * Надо сделать цикл по всем дням, где есть люди.*/
+    sql.main("SELECT date FROM qdriver UNION SELECT date FROM qpassenger;", function (error, rows) { 
+        //Цикл по всем направлениям.
+        for (var direction = 1; direction <= Var.directionSize; direction++) {
+            //Цикл по временам - на каждом интервале по 2 очереди: пассажиры и водители
+            //всего 8 интервалов времени с 00:00 - 03:00, 03:00 - 06:00, и т.д с шагом в 3 часа
+            //Кстати. не факт, что с шагом в 3 часа. 
+            for (var TIME = 1; TIME <= 8; TIME++) {
+                //Если в очереди никого нет
+                if (Var.qDriver[direction][TIME].length * Var.qPassenger[direction][TIME].length == 0) {
+                } else
+                    console.log("time: ", TIME, " direction: ", direction, " queue's size: ", 
                     Var.qDriver[direction][TIME].length + Var.qPassenger[direction][TIME].length);
-            //Цикл по всем Пассажирам
-            for (var iPass = 0; iPass < Var.qPassenger[direction][TIME].length; iPass++) {
-                var booked = Var.qPassenger[direction][TIME][iPass]["booked"] - 0;   //Сколько мест забронировать
-                var passengerID = Var.qPassenger[direction][TIME][iPass]["id"] - 0;  //ID пассажира
-                
-                //Цикл по всем водителям
-                for(var iDrive = 0; iDrive < Var.qDriver[direction][TIME].length && booked > 0; iDrive++) {
-                    var driverID = Var.qDriver[direction][TIME][iDrive]["id"] - 0;       //ID водителя
-                    var driverSeats = Var.qDriver[direction][TIME][iDrive]["seats"] - 0; //Сколько свободных мест имеется у водителя
-
-                    if (booked <= driverSeats) {
-                        // можно посадить пассажира на эту машину
-                        //Место, куда посылаем встретившихся
-                        Var.met[direction][TIME].push({ id: passengerID, driversPhone: driverID });
-
-                        //Кидаем в БД запись о встрече пассажира(ов) и водителя
-                        sql.main("INSERT INTO met(id_driver, id_passenger, id_direction, id_time)" +
-                            "VALUES(" + driverID + ", " + passengerID + ", " + direction + ", " + TIME + ");", function (error, rows){
+                //Цикл по всем Пассажирам
+                for (var iPass = 0; iPass < Var.qPassenger[direction][TIME].length; iPass++) {
+                    var booked = Var.qPassenger[direction][TIME][iPass]["booked"] - 0;   //Сколько мест забронировать
+                    var passengerID = Var.qPassenger[direction][TIME][iPass]["id"] - 0;  //ID пассажира
+                    
+                    //Цикл по всем водителям
+                    for (var iDrive = 0; iDrive < Var.qDriver[direction][TIME].length && booked > 0; iDrive++) {
+                        var driverID = Var.qDriver[direction][TIME][iDrive]["id"] - 0;       //ID водителя
+                        var driverSeats = Var.qDriver[direction][TIME][iDrive]["seats"] - 0; //Сколько свободных мест имеется у водителя
+                        
+                        if (booked <= driverSeats) {
+                            // можно посадить пассажира на эту машину
+                            //Место, куда посылаем встретившихся
+                            Var.met[direction][TIME].push({ id: passengerID, driversPhone: driverID });
+                            
+                            //Кидаем в БД запись о встрече пассажира(ов) и водителя
+                            sql.main("INSERT INTO met(id_driver, id_passenger, id_direction, id_time)" +
+                            "VALUES(" + driverID + ", " + passengerID + ", " + direction + ", " + TIME + ");", function (error, rows) {
                                 if (error) {
                                     console.log("errorDB: adding driver after meeting to MetTable(DB)");
                                 }
                             });
-
-                        //Уменьшаем количество свободных мест в машине.
-                        Var.qDriver[direction][TIME][iDrive]["seats"] = driverSeats - booked;
-                        //Изменяем запись в Базе Данных:
-                        sql.main("UPDATE qdriver SET seats = " + Var.qDriver[direction][TIME][iDrive]["seats"] + " WHERE id_driver = " + 
+                            
+                            //Уменьшаем количество свободных мест в машине.
+                            Var.qDriver[direction][TIME][iDrive]["seats"] = driverSeats - booked;
+                            //Изменяем запись в Базе Данных:
+                            sql.main("UPDATE qdriver SET seats = " + Var.qDriver[direction][TIME][iDrive]["seats"] + " WHERE id_driver = " + 
                             driverID + " AND id_time = " + TIME + " AND id_direction = " + direction + ";", 
                             function (error, rows) {
                                 if (error) console.log("errorDB: could not update seats");
-                        });
-
-                        //Выставляем количество забронированных мест у пассажира в ноль, ибо он только что сел
-                        Var.qPassenger[direction][TIME][iPass]["booked"] = 0;
-                        
-                        //Меняем запись в Базе Данных, ставим 0 на количестве забронированных мест
-                        sql.main("UPDATE qpassenger SET booked = 0 WHERE id_passenger = " + passengerID + " AND id_direction = " + direction 
+                            });
+                            
+                            //Выставляем количество забронированных мест у пассажира в ноль, ибо он только что сел
+                            Var.qPassenger[direction][TIME][iPass]["booked"] = 0;
+                            
+                            //Меняем запись в Базе Данных, ставим 0 на количестве забронированных мест
+                            sql.main("UPDATE qpassenger SET booked = 0 WHERE id_passenger = " + passengerID + " AND id_direction = " + direction 
                             + " AND id_time = " + TIME + ";", function (error, rows) {
-                            if (error) console.log("errorDB: could update qpassenger.booked to zero");
-                        });
-                        //Если Пассажира посадили, то смысл дальше идти по водителям?
-                        booked = 0;
-                    }
+                                if (error) console.log("errorDB: could update qpassenger.booked to zero");
+                            });
+                            //Если Пассажира посадили, то смысл дальше идти по водителям?
+                            booked = 0;
+                        }
                     //end:if (booked <= driverSeats)
-                }
+                    }
                 //end: Цикл по водителям
-            }
+                }
             //end: Цикл по Пассажирам
-        }
+            }
         //end: Цикл по временным промежуткам
-    }
+        }
     //end: Цикл по направлениям
+    });
+    //end: Цикл по дням, в очереди которых есть кто-либо    
 }
 
 //Неплохо было бы в отдельную функцию вывести процесс удаления с очереди, в случае, 
@@ -113,4 +119,4 @@ var clear = function clearQueue() {
         }
 }
 
-exports.find = find;
+exports.main = mainF;
