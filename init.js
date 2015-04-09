1583bc66d16815e7005310c5c6b3a5fcf69610fa
ilnuirbat <ilnuribat@gmail.com>
2015-04-10 03:17:09 +0500

    set Var.directionSize variable, cause big problems were found in MakeQueueNoDB

---
+++ init.js
@@ -0,0 +1,6 @@
+var Var = require('./variables.js');
+var sql = require('./sql.js');
+
+sql.main("SELECT COUNT(id) AS 'count' FROM direction;", function (error, rows) {
+    Var.directionSize = rows[0]["count"];
+});
