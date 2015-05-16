d541dd6512b2cabbb0acdeac32d55f6df4504c42
ilnuirbat <ilnuribat@gmail.com>
2015-05-16 11:49:33 +0500

    direction size not actual

--- init.js
+++ init.js
@@ -3,5 +3,4 @@ var sql = require('./sql.js');
 
 sql.main("SELECT COUNT(id) AS 'count' FROM direction;", function (error, rows) {
     Var.directionSize = rows[0]["count"];
-	console.log(Var.directionSize, ": direction size");
 });
