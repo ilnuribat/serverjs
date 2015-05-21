540071d191298ca1419e23e494d7c72a4278bb3d
ilnur <ilnuribat@gmail.com>
2015-05-21 10:00:02 +0500

    SQL REQUEST WITH UTF8

--- CrashTest.cmd
+++ CrashTest.cmd
@@ -1,3 +1,13 @@
-:loop
-curl localhost/registration -d "human=driver&name=TestTime&phone=1"
-goto loop
+use server;
+
+SET character_set_server=utf8;
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+SET character_set_results=utf8;
+
+SELECT towns.name, dest.name, date, seats, driver.name 
+FROM qdriver 
+INNER JOIN direction ON direction.id = id_direction 
+INNER JOIN towns ON towns.id = direction.id_source 
+INNER JOIN towns as dest ON dest.id = direction.id_destination 
+INNER JOIN driver ON driver.id = id_driver;
