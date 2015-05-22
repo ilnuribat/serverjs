1a06d28e01601cc353935cef2c86574e03ed346b
ilnuirbat <ilnuribat@gmail.com>
2015-05-22 16:30:45 +0500

    added time.name column

--- CrashTest.cmd
+++ CrashTest.cmd
@@ -5,9 +5,10 @@ SET character_set_client=utf8;
 SET character_set_connection=utf8;
 SET character_set_results=utf8;
 
-SELECT towns.name, dest.name, date, seats, driver.name 
+SELECT towns.name, dest.name, time.name, date, seats, driver.name 
 FROM qdriver 
 INNER JOIN direction ON direction.id = id_direction 
 INNER JOIN towns ON towns.id = direction.id_source 
 INNER JOIN towns as dest ON dest.id = direction.id_destination 
-INNER JOIN driver ON driver.id = id_driver;
+INNER JOIN driver ON driver.id = id_driver
+INNER JOIN time ON time.id = id_time;
