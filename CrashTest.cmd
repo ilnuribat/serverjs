1d3029cd50d69bdee051f8f3c2e3bf021e7fab0f
ilnuirbat <ilnuribat@gmail.com>
2015-05-23 20:49:06 +0500

    added also for qpassengers

--- CrashTest.cmd
+++ CrashTest.cmd
@@ -9,3 +9,11 @@ INNER JOIN towns ON towns.id = direction.id_source
 INNER JOIN towns as dest ON dest.id = direction.id_destination 
 INNER JOIN driver ON driver.id = id_driver
 INNER JOIN time ON time.id = id_time;
+
+SELECT towns.name, dest.name, time.name, date, booked, passenger.name 
+FROM qpassenger 
+INNER JOIN direction ON direction.id = id_direction 
+INNER JOIN towns ON towns.id = direction.id_source 
+INNER JOIN towns as dest ON dest.id = direction.id_destination 
+INNER JOIN passenger ON passenger.id = id_passenger
+INNER JOIN time ON time.id = id_time;
