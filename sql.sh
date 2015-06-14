24060f580df6a8142273992ed898e8f87a89b747
ilnuribat <ilnuribat@gmail.com>
2015-06-14 07:55:42 +0600

    make dataBase only for UnderUfa, no passengers, no server.met

--- sql.sh
+++ sql.sh
@@ -1,10 +1,6 @@
 DROP DATABASE server;
 CREATE DATABASE server CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 use server
-CREATE TABLE time(
-    id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    name VARCHAR(20)
-);
 
 CREATE TABLE towns(
     id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -24,49 +20,16 @@ CREATE TABLE driver(
     id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	name VARCHAR(20) NOT NULL COLLATE utf8_unicode_ci,
     phone VARCHAR(11) NOT NULL,
-    access INTEGER,
 	UNIQUE KEY uniquePhone(phone)	
-)CHARACTER SET utf8 COLLATE utf8_general_ci;
-
-CREATE TABLE passenger(
-    id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    name VARCHAR(20) NOT NULL COLLATE utf8_unicode_ci,
-    phone VARCHAR(11) NOT NULL,
-	UNIQUE KEY uniquePhone(phone)
 )CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 CREATE TABLE qdriver(
     id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
     id_driver INTEGER,
-    id_time INTEGER,
     id_direction INTEGER,
-    seats INTEGER,
-    date INTEGER,
-	UNIQUE KEY uniqueDriverInQueue(id_driver, id_time, id_direction, date),
+    date DATE,
+	time VARCHAR(5),
+	UNIQUE KEY uniqueDriverInQueue(id_driver, id_direction, date, time),
     FOREIGN KEY (id_driver) REFERENCES driver(id),
-    FOREIGN KEY (id_time)REFERENCES  time(id),
     FOREIGN KEY (id_direction)REFERENCES  direction(id)
-);
-
-CREATE TABLE qpassenger(
-    id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    id_passenger INTEGER,
-    id_time INTEGER,
-    id_direction INTEGER,
-    booked INTEGER,
-    date INTEGER,
-	UNIQUE KEY uniquePassengerInQueue(id_passenger, id_time, id_direction, date),
-    FOREIGN KEY (id_passenger) REFERENCES passenger(id),
-    FOREIGN KEY (id_time)REFERENCES  time(id),
-    FOREIGN KEY (id_direction)REFERENCES  direction(id)
-);
-CREATE TABLE met(
-	id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-	id_driver INTEGER,
-	id_passenger INTEGER,
-	id_direction INTEGER,
-	id_time INTEGER,
-	booked INTEGER,
-	date INTEGER,
-	UNIQUE KEY uniqueMet(id_driver, id_passenger, id_direction, id_time, date)
 );
