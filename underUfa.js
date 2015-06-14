70f13d32738ac4f36df42d5c90b2b3cd457f596a
ilnuribat <ilnuribat@gmail.com>
2015-06-14 08:04:21 +0600

    get requests of our new functions

---
+++ underUfa.js
@@ -0,0 +1,10 @@
+﻿var Var = require('./variables.js');
+var sql = require('./sql.js');
+
+Var.app.get('/driversList', function (request, response) {
+    var query = Var.url.parse(request.url).query;
+    var params = Var.queryString.parse(query);
+    var direction = params["direction"];
+    var date = params["date"];
+
+});
