8303f63e964a6217e0a6481fdd6d83b461e05698
ilnuribat <ilnuribat@gmail.com>
2015-07-05 20:15:44 +0500

    check DB for ready to work

--- init.js
+++ init.js
@@ -2,6 +2,10 @@ var Var = require('./variables.js');
 var sql = require('./sql.js');
 
 sql.main("SELECT MAX(id) AS 'max', MIN(id) AS 'min' FROM direction;", function (error, rows) {
+	if(error) {
+		console.log(new Date().toLocaleString(), "the DataBase is not initialized");
+		return;
+	}
     Var.directionMax = rows[0]["max"];
     Var.directionMin = rows[0]["min"];
 });
